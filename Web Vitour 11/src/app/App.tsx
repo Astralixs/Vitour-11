54def06f46eb90d55d30525f1677fb43b0ff0bf2
StarrySky <m.faizfadhillah1@gmail.com>
2026-07-28 08:08:48 +0700

    Admin Login

--- Web Vitour 11/src/app/App.tsx
+++ Web Vitour 11/src/app/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router';
 import HomePage from './components/HomePage';
 import TourPage from './components/TourPage';
-import AdminPanel from './components/ui/AdminPanel';
+import AdminPanel from './components/AdminPanel';
 
 export default function App() {
   return (
