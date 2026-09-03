12269d0e4d4b1f25fb804f2a3336c93df63b5512
StarrySky <m.faizfadhillah1@gmail.com>
2026-09-03 09:11:09 +0700

    cleaned files

--- Web Vitour 11/src/app/App.tsx
+++ Web Vitour 11/src/app/App.tsx
@@ -1,9 +1,9 @@
 import { BrowserRouter, Routes, Route } from 'react-router';
-import HomePage from './components/HomePage';
-import TourPage from './components/TourPage';
-import Admin from './components/AdminPanel';
-import Details from './components/DetailRoom';
-import RoomMap from './components/RoomMap';
+import HomePage from './components/User/HomePage';
+import TourPage from './components/User/TourPage';
+import Admin from './components/Admin/AdminPanel';
+import Details from './components/User/DetailRoom';
+import RoomMap from './components/User/RoomMap';
 
 export default function App() {
   return (
@@ -14,7 +14,7 @@ export default function App() {
         <Route path="/tour" element={<TourPage />} />
         <Route path="/Details" element={<RoomMap />} />
         <Route path="/Details/:id" element={<Details />} />
-        
+
         {/* Admins */}
         <Route path="/Admin-Panels" element={<Admin />} />
       </Routes>
